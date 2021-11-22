356c97fbbd124474840dc20cad35868711c665f1
Isaac Hernandez <isdhdezc97@gmail.com>
2021-11-22 13:57:43 +0000

    Body Form Added

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,15 +2,18 @@ import React from "react";
 
 //include images into your bundle
 import CardBody from "./CardBody.jsx";
+import BodyForm from "./BodyForm.jsx";
 //create your first component
 const Home = () => {
 	return (
-		<div className="container">
+		<div className="container-fluid">
 			<div className="row"></div>
-			<div className="row">
+			<div className="row justify-content-center">
 				<CardBody></CardBody>
 			</div>
-			<div className="row"></div>
+			<div className="row">
+				<BodyForm></BodyForm>
+			</div>
 			<div className="row"></div>
 		</div>
 	);
